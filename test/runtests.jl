780f9d91452fa0d7cf6c7469870e17b25c0cdfea
abdelazimhussien <abdelazim.hussien@liu.se>
2025-02-21 13:24:25 +0100

    my_f

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,8 @@ using MetaheuristicsAlgorithms
 using Test
 
 
-my_f(2,1)
+# my_f(2,1)
 @testset "MetaheuristicsAlgorithms.jl" begin
     # Write your tests here.
-    my_f(2,1)
+    # my_f(2,1)
 end
