e4d6d1038eabd76abce364938e3b5da60937231b
karei <abcdvvvv@126.com>
2025-06-10 22:04:51 +0900

    modified:   test/runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,11 @@ using MetaheuristicsAlgorithms
 using Test
 using Random
 
-Random.seed!(1)
 include("benchmark.jl")
 
 @testset "MetaheuristicsAlgorithms.jl" begin
-    @testset "AEFA on EGGHOLDER FUNCTION" begin
+    @testset "AEFA on Eggholder function" begin
+        Random.seed!(1)
         lb = [-512, -512]
         ub = [512, 512]
         algo = AEFA(100, 300, lb, ub)
@@ -17,5 +17,16 @@ include("benchmark.jl")
         end
         @test algo.gbest == [-461.648054120324, -390.06329199871277]
         @test algo.gbest_val[end] == -775.8393103361666
+    end
+
+    @testset "AEO on Eggholder function" begin
+        Random.seed!(1)
+        lb = [-512, -512]
+        ub = [512, 512]
+        fun(x) = first(benchmark(x))
+        BestF, BestX, _ = AEO(100, 300, lb, ub, 2, fun)
+
+        @test BestF == -959.640662720851
+        @test BestX == [512.0, 404.2318050941216]
     end
 end
