1b40d67f9e9bec43012881fcf44dfd52970dc590
jbytecode <mhsatman@gmail.com>
2025-06-13 11:28:09 +0300

    introduce idiomatic Julia for tests

---
+++ test/testdso.jl
@@ -0,0 +1,23 @@
+@testset "DSO" verbose = true begin 
+    @testset "Ackley with DSO, p = 2" verbose = true begin 
+        lb = [-32.768 for i in 1:2]
+        ub = [32.768 for i in 1:2]
+        result = DSO(100, 500, lb, ub, Ackley)
+        @test isapprox(result.Best_Position, [0.0 for i in 1:2], atol=1e-5)
+        @test isapprox(result.Best_Cost, 0.0, atol=1e-5)
+    end 
+    @testset "Ackley with DSO, p = 5" verbose = true begin 
+        lb = [-32.768 for i in 1:5]
+        ub = [32.768 for i in 1:5]
+        result = DSO(100, 500, lb, ub, Ackley)
+        @test isapprox(result.Best_Position, [0.0 for i in 1:5], atol=1e-5)
+        @test isapprox(result.Best_Cost, 0.0, atol=1e-5)
+    end
+    @testset "Griewank with DSO, p = 2" verbose = true begin 
+        lb = [-600 for i in 1:2]
+        ub = [600 for i in 1:2]
+        result = DSO(100, 500, lb, ub, Griewank)
+        @test isapprox(result.Best_Position, [0.0 for i in 1:2], atol=1e-5)
+        @test isapprox(result.Best_Cost, 0.0, atol=1e-5)
+    end
+end 
