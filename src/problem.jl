8a8f0c26042fc3e8f89c90c83120301bfc691b06
karei <abcdvvvv@126.com>
2025-06-23 14:32:15 +0900

    modified:   src/problem.jl 	modified:   test/testalgo/testaeostruct.jl 	modified:   test/testplot.jl

--- src/problem.jl
+++ src/problem.jl
@@ -1,19 +1,16 @@
-
-# module ProblemDefinition
-
 export OptimizationProblem
 
-struct OptimizationProblem
-    objfun::Function
-    lb::Union{Real, AbstractVector{<:Real}}
-    ub::Union{Real, AbstractVector{<:Real}}
+struct OptimizationProblem{F}
+    objfun::F
+    lb::Vector{Float64}
+    ub::Vector{Float64}
     dim::Int
-
-    # function OptimizationProblem(f::Function, lb, ub, dim::Int)
-    #     return OptimizationProblem(f, lb, ub, dim)
-    # end
-    
 end
 
+function OptimizationProblem(f::F, lb::Vector{Float64}, ub::Vector{Float64}, dim::Int=length(lb)) where {F}
+    length(lb) == length(ub) == dim || throw(DimensionMismatch("lb, ub, and dim must have the same length"))
+    OptimizationProblem{F}(f, lb, ub, dim)
+end
 
-# end
+OptimizationProblem(f::F, lb::Real, ub::Real, dim::Int=1) where {F} =
+    OptimizationProblem{F}(f, fill(lb, dim), fill(ub, dim), dim)
