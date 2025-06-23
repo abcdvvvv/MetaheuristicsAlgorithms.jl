e55e4399ad392823d89ddf0bc1acff2b9e0f1c03
karei <abcdvvvv@126.com>
2025-06-23 14:35:42 +0900

    modified:   src/problem.jl

--- src/problem.jl
+++ src/problem.jl
@@ -7,10 +7,10 @@ struct OptimizationProblem{F}
     dim::Int
 end
 
-function OptimizationProblem(f::F, lb::Vector{Float64}, ub::Vector{Float64}, dim::Int=length(lb)) where {F}
+function OptimizationProblem(f::F, lb::Vector{T}, ub::Vector{T}, dim::Integer=length(lb)) where {F,T<:Real}
     length(lb) == length(ub) == dim || throw(DimensionMismatch("lb, ub, and dim must have the same length"))
     OptimizationProblem{F}(f, lb, ub, dim)
 end
 
-OptimizationProblem(f::F, lb::Real, ub::Real, dim::Int=1) where {F} =
+OptimizationProblem(f::F, lb::Real, ub::Real, dim::Integer=1) where {F} =
     OptimizationProblem{F}(f, fill(lb, dim), fill(ub, dim), dim)
