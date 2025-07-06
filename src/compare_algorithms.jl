4fa41b5830e6cc54b75b6f2939e89d625339bcdc
karei <abcdvvvv@126.com>
2025-07-06 21:56:47 +0900

    modified:   src/compare_algorithms.jl

--- src/compare_algorithms.jl
+++ src/compare_algorithms.jl
@@ -1,14 +1,14 @@
 function compare_algorithms(
     algorithms::Vector{<:Function},
     problem::OptimizationProblem;
-    npop::Int = 30,
-    max_iter::Int = 1000
+    npop::Integer = 30,
+    max_iter::Integer = 1000
 )
     results = Dict{String, OptimizationResult}()
 
     for algo in algorithms
         name = string(nameof(algo))
-        sol = optimize(algo, problem; npop=npop, max_iter=max_iter)
+        sol = optimize(algo, problem; npop, max_iter)
         results[name] = sol
     end
 
